dad0b813d99b5d30f873c93ffbca990e0441643d
Yusupov Nizomiddin <yusupov1995niko@gmail.com>
2021-11-16 14:42:17 +0500

    tekshirish uchun

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
   console.log()
   console.log('Nizomiddin men Sizdi proektingizga o\'zgartirish kiritdim')
   console.log('Ha ko\'rdim o\zgartirganingizni!!!')
+  console.log("yangilandimi domla?????")
 })()
